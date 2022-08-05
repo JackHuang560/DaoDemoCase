19fc183c20492c8e83b9e9573e7103a316c483bf
林志興 <shingolin@gmail.com>
2022-08-05 10:23:24 +0800

    加入專案檔案。

---
+++ DaoDemo/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(196, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(1495, 594);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(626, 762);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Query-Lambda";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(906, 762);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 71);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Query-SqlLikte";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1174, 767);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 57);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Ado";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(556, 912);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(711, 29);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2162, 1141);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ DaoDemo/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DbDataContext db = new DbDataContext();
+            var data = db.ABS.Where(p => p.BDATE >= new DateTime(2022, 1, 1) && p.BDATE <= new DateTime(2022, 1, 31));
+            data = data.Where(p => p.NOBR == "H000003");
+            var hours = data.First().TOL_HOURS;
+            MessageBox.Show(hours.ToString());
+            dataGridView1.DataSource = data;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DbDataContext db = new DbDataContext();
+            var data = from a in db.ABS
+                       join b in db.BASE on a.NOBR equals b.NOBR
+                       where a.BDATE >= new DateTime(2022, 1, 1) && a.BDATE <= new DateTime(2022, 1, 31)
+                       select new TestDto { 員工編號 = a.NOBR, 員工姓名 = b.NAME_C, 請假日期 = a.BDATE, 開始時間 = a.BTIME, 結束時間 = a.ETIME };
+            var value = 1234;
+            dataGridView1.DataSource = data;
+        }
+        public List<TestDto> getData()
+        {
+            DbDataContext db = new DbDataContext();
+            var data = from a in db.ABS
+                       join b in db.BASE on a.NOBR equals b.NOBR
+                       where a.BDATE >= new DateTime(2022, 1, 1) && a.BDATE <= new DateTime(2022, 1, 31)
+                       select new TestDto { 員工編號 = a.NOBR, 員工姓名 = b.NAME_C, 請假日期 = a.BDATE, 開始時間 = a.BTIME, 結束時間 = a.ETIME };
+            return data.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //MessageBox.Show(Properties.Settings.Default.EMCCNConnectionString);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var sqlconnection = new SqlConnection(Properties.Settings.Default.EMCCNConnectionString);
+            using (sqlconnection)
+            {
+                if (sqlconnection.State != ConnectionState.Open)
+                    sqlconnection.Open();
+
+                //SqlCommand sqlCommand = new SqlCommand(@"select * from ABS 
+                //                    where BDATE>='20220101' and BDATE<='20220131'
+                //                    and NOBR='H000003'", sqlconnection);
+
+                SqlCommand sqlCommand = new SqlCommand(textBox1.Text, sqlconnection);
+
+                var dr = sqlCommand.ExecuteReader();
+                var dataTable = new DataTable("ABS");
+                dataTable.Load(dr);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+    }
+    public class TestDto
+    {
+        public string 員工編號 { get; set; }
+        public string 員工姓名 { get; set; }
+        public DateTime 請假日期 { get; set; }
+        public string 開始時間 { get; set; }
+        public string 結束時間 { get; set; }
+    }
+}
